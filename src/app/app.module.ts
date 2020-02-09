5bcfa7e4f73a3a0c8fe440ed02bd4f4538850d3f
Will <doggy.huang@gmail.com>
2020-02-09 11:49:17 +0800

    建立 SocialIconsComponent 元件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { SocialIconsComponent } from './header/social-icons/social-icons.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SocialIconsComponent
   ],
   imports: [
     BrowserModule,
