3b25c94c5577e0984abe0613672485bd4eda40a2
Will <doggy.huang@gmail.com>
2020-02-09 16:54:43 +0800

    Revert "示範使用 async Pipe 元件訂閱 Observable 物件"
    This reverts commit 8744a0f9dea072747e451669c14054ef7fde6669.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { share } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -11,12 +9,15 @@ import { share } from 'rxjs/operators';
 export class AppComponent implements OnInit {
   keyword = 'demo1';
 
-  data$: Observable<any>;
+  data: any;
 
   constructor(private http: HttpClient) {
   }
 
   ngOnInit(): void {
-    this.data$ = this.http.get('/api/articles.json').pipe(share());
+    this.http.get('/api/articles.json')
+      .subscribe((value) => {
+        this.data = value;
+      });
   }
 }
