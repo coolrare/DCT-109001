0d773ccd3c09b4f52a29989a56fa4de5d4bba084
Will <doggy.huang@gmail.com>
2020-02-09 13:50:54 +0800

    將 imgsrc 更名為 sitelogo

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,7 +9,7 @@ export class HeaderComponent implements OnInit {
 
   sitename = 'Hello World';
   subtitle = '記載著 Will 在網路世界的學習心得與技術分享';
-  imgsrc = '/assets/images/logo.png';
+  sitelogo = '/assets/images/logo.png';
 
   showIcons = false;
 
